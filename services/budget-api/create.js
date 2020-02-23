eb8e91b439139056492b5d627e34e7018b40df47
wc0sby <wcosby6607@gmail.com>
2020-02-23 09:29:02 -0600

    adding budget services

---
+++ services/budget-api/create.js
@@ -0,0 +1,24 @@
+import uuid from "uuid/v1";
+import * as dynamoDbLib from "../../libs/dynamodb-lib";
+import { success, failure } from "../../libs/response-lib";
+
+export async function main(event, context) {
+  const data = JSON.parse(event.body);
+  const params = {
+    TableName: "budget-entries",
+    Item: {
+      userId: event.requestContext.identity.cognitoIdentityId,
+      budgetId: uuid(),
+      content: data.content,
+      attachment: data.attachment,
+      createdAt: Date.now()
+    }
+  };
+
+  try {
+    await dynamoDbLib.call("put", params);
+    return success(params.Item);
+  } catch (e) {
+    return failure({ status: false });
+  }
+}
